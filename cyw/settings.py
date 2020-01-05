4eced2db3b0e644a35f2bb4d2db0583bc7bb5b96
armando-femat <armando.femat@tutanota.com>
2020-01-05 14:16:11 +0100

    Creation du fichier base.html

--- cyw/settings.py
+++ cyw/settings.py
@@ -124,12 +124,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "compare/Static"),
+    os.path.join(BASE_DIR, "compare/static"),
 ]
 # Python package for forms template (register)
 CRYSPY_TEMPLATE_PACK = 'bootstrap4'
