448b6eecc99a8315186c2cd39b1ac75888cdd710
armando-femat <armando.femat@tutanota.com>
2020-01-19 22:49:47 +0100

    Added profile options: is list, ville and autres. Still need to correct the form to be able to create profiles

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,72 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from compare.models import Ville
+from dal import autocomplete
+from users.models import Profile
 
 
+class CustomUserCreationForm(forms.Form):
+    username = forms.CharField(label='Nom d\'utilisateur', min_length=3, max_length=20)
+    email = forms.EmailField(label='Email')
+    password1 = forms.CharField(label='Mot de passe', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmation du mot de passe', widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise ValidationError("Le nom d\'utilisateur existe déjà !")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise ValidationError('Email existe déjà !')
+        return email
+
+    def clean_password(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError('Mot de passe différents !')
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1'],
+            self.cleaned_data['ville'],
+            self.cleaned_data['is_list']
+
+        )
+        return user
+
+
+class ProfileRegisterForm(forms.Form):
+    is_list = forms.BooleanField(label='Appartients tu a une liste ?', required=False, initial=False,
+                                 widget=forms.CheckboxInput)
+
+    ville = forms.ModelChoiceField(queryset=Ville.objects.all(),
+                                   widget=autocomplete.ModelSelect2(url='ville-autocomplete'))
+    ville.widget.attrs.update({'class': 'form-home'})
+
+"""
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not email:
+            raise ValidationError("This field is required.")
+        if User.objects.filter(email=self.cleaned_data['email']).count():
+            raise ValidationError("Email is taken.")
+        return self.cleaned_data['email']
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+"""
--- users/models.py
+++ users/models.py
@@ -1,10 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from compare.models import Ville
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
+    # Necessary information
+    # User includes ()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(null=True)
+    is_list = models.BooleanField(default=False)
+    ville = models.ForeignKey(Ville, on_delete=models.PROTECT, null=True)
+
+    # Updating your user with other things that are not really necessary.
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def __str__(self):        return f'{self.user.username} Profile'
+    # Additional fields
+    activation_key = models.CharField(max_length=255, default=1)
+    email_validated = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
